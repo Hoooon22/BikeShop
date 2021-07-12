4b77c58e3ebf40a4acb5b6ec5f12c2e694d50b25
Hoooon22 <momo990305@gmail.com>
2021-07-12 23:21:46 +0900

    sync

--- Contact.xaml.cs
+++ Contact.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Shapes;
 
 namespace BikeShop
 {
-    /// <summary>
+    /// <summary> 
     /// Contact.xaml에 대한 상호 작용 논리
     /// </summary>
     public partial class Contact : Page
